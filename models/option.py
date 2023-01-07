cad7c3e4fdbc70f7879eba64d9e3250c357b03b0
eerieA <ninja.duan@gmail.com>
2023-01-06 16:04:49 -0800

    Fixed missing ()s.

--- models/option.py
+++ models/option.py
@@ -1,4 +1,6 @@
 from typing import List
+import datetime
+import pytz
 from models.connection_pool import get_connection
 import database
 
@@ -12,22 +14,24 @@ class Option:
         return f"Poll({self.text!r}, {self.poll_id!r}, {self.id!r})"
     
     def save(self):
-        with get_connection as connection:
+        with get_connection() as connection:
             new_option_id = database.add_option(connection, self.text, self.poll_id)
             self.id = new_option_id
 
     @classmethod
     def get(cls, option_id:int) -> "Option":
-        with get_connection as connection:
+        with get_connection() as connection:
             option = database.get_option(connection, option_id)
             return cls(option[1], option[2], option[0])
 
     def vote(self, username:str):
-        with get_connection as connection:
-            database.add_poll_vote(connection, username, self.id)
+        with get_connection() as connection:
+            current_datetime_utc = datetime.datetime.now(tz=pytz.utc)
+            current_timestamp_utc = current_datetime_utc.timestamp()
+            database.add_poll_vote(connection, username, current_timestamp_utc, self.id)
 
     @property
     def votes(self) -> list[database.Vote]:
-        with get_connection as connection:
+        with get_connection() as connection:
             votes = database.get_votes_for_option(connection, self.id)
             return votes
--- models/poll.py
+++ models/poll.py
@@ -13,7 +13,7 @@ class Poll:
         return f"Poll({self.title!r}, {self.owner!r}, {self.id!r})"
 
     def save(self):
-        with get_connection as connection:
+        with get_connection() as connection:
             new_poll_id = database.create_poll(connection, self.title, self.owner)
             self.id = new_poll_id
 
@@ -22,24 +22,24 @@ class Poll:
 
     @property
     def options(self) -> list[Option]:
-        with get_connection as connection:
+        with get_connection() as connection:
             options = database.get_poll_options(connection, self.id)
             return [Option(option[1], option[2], option[0]) for option in options]
 
     @classmethod
     def get(cls, poll_id:int) -> "Poll":
-        with get_connection as connection:
+        with get_connection() as connection:
             poll = database.get_poll(connection, poll_id)
             return cls(poll[1], poll[2], poll[0])
 
     @classmethod
     def all(cls) -> list["Poll"]:
-        with get_connection as connection:
+        with get_connection() as connection:
             polls = database.get_polls(connection)
             return [cls(poll[1], poll[2], poll[0]) for poll in polls]
 
     @classmethod
     def latest(cls) -> "Poll":
-        with get_connection as connection:
+        with get_connection() as connection:
             poll = database.get_latest_poll(connection)
             return cls(poll[1], poll[2], poll[0])
